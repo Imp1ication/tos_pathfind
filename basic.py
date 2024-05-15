eac67d30994c015daac67cac713852a215176bf2
Imp1ication <1mplicationxwzi@gmail.com>
2024-05-15 15:04:27 +0800

    rewrite class Move, change list value to tuple

--- basic.py
+++ basic.py
@@ -2,10 +2,10 @@ from enum import Enum
 
 
 class Move(Enum):
-    UP = [-1, 0]
-    DOWN = [1, 0]
-    LEFT = [0, -1]
-    RIGHT = [0, 1]
+    UP = (-1, 0)
+    DOWN = (1, 0)
+    LEFT = (0, -1)
+    RIGHT = (0, 1)
 
 
 class StoneType(Enum):
